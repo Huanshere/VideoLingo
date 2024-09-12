dfbe4d6e7bc3f984c9c48ee55dfbb62582d52ae4
HUANYU XU <147359634+Huanshere@users.noreply.github.com>
2024-09-12 14:25:21 +0800

    update

--- core/step4_1_summarize.py
+++ core/step4_1_summarize.py
@@ -31,6 +31,7 @@ def get_summary():
     from config import step4_1_summarize_model
     src_content = combine_chunks()
     summary_prompt = get_summary_prompt(src_content)
+    print(f"📝 Summarizing... Please wait a moment...")
     summary = ask_gpt(summary_prompt, model=step4_1_summarize_model, response_json=True, valid_key='terms', log_title='summary')
 
     with open('output/log/terminology.json', 'w', encoding='utf-8') as f:
