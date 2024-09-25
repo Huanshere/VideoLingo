132e0d429c473310b21f6b6b9c473cb91a142756
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2024-09-25 21:21:06 +0800

    chore: SSG

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,16 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
+    themeConfig: './theme.config.jsx',
 })
 
-module.exports = withNextra()
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
+}
+
+module.exports = withNextra(nextConfig)
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
