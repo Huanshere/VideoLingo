03b3a2e4a7acc6fc5032b7cca3544763e5bbedb2
yxc0915 <62410385+yxc0915@users.noreply.github.com>
2024-10-06 19:52:49 +0800

    Update pypi_autochoose.py

--- core/pypi_autochoose.py
+++ core/pypi_autochoose.py
@@ -18,7 +18,6 @@ mirrors = {
     "阿里云": "https://mirrors.aliyun.com/pypi/simple",
     "清华大学": "https://pypi.tuna.tsinghua.edu.cn/simple",
     "中国科技大学": "https://pypi.mirrors.ustc.edu.cn/simple",
-    "豆瓣": "https://pypi.doubanio.com/simple",
     "华为云": "https://repo.huaweicloud.com/repository/pypi/simple",
     "腾讯云": "https://mirrors.cloud.tencent.com/pypi/simple",
     "Google": "https://pypi.org/simple",
