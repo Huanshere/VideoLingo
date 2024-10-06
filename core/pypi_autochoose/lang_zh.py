6a2d30eb0818d863b8271c29f78a770c26112c9e
yxc0915 <62410385+yxc0915@users.noreply.github.com>
2024-10-07 00:54:49 +0800

    Update lang_zh.py

--- core/pypi_autochoose/lang_zh.py
+++ core/pypi_autochoose/lang_zh.py
@@ -5,6 +5,7 @@ MIRRORS = {
     "中国科技大学": "https://mirrors.ustc.edu.cn/pypi/simple",
     "华为云": "https://repo.huaweicloud.com/repository/pypi/simple",
     "腾讯云": "https://mirrors.cloud.tencent.com/pypi/simple",
+    "上海交通大学": "https://mirror.sjtu.edu.cn/pypi/web/simple"
 }
 
 MESSAGES = {
