cbc2af9d180948aeb316b854e4904820c2315985
yxc0915 <62410385+yxc0915@users.noreply.github.com>
2024-10-06 22:09:33 +0800

    Update lang_zh.py

--- core/pypi_autochoose/lang_zh.py
+++ core/pypi_autochoose/lang_zh.py
@@ -1,9 +1,8 @@
 MIRRORS = {
     "PyPI官方": "https://pypi.org/simple",
-    "阿里云": "https://mirrors.aliyun.com/pypi/simple",
-    "清华大学": "https://pypi.tuna.tsinghua.edu.cn/simple",
-    "中国科技大学": "https://pypi.mirrors.ustc.edu.cn/simple",
-    "豆瓣": "https://pypi.doubanio.com/simple",
+    "阿里云": "http://mirrors.aliyun.com/pypi/simple",
+    "清华大学": "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple",
+    "中国科技大学": "https://mirrors.ustc.edu.cn/pypi/simple",
     "华为云": "https://repo.huaweicloud.com/repository/pypi/simple",
     "腾讯云": "https://mirrors.cloud.tencent.com/pypi/simple",
     "Google": "https://pypi.org/simple",
