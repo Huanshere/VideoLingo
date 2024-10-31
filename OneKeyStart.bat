8e033a8ac2dc672888850e63d47abe75f0c14f6c
HUANYU XU <xuhuanyu2069@gmail.com>
2024-10-31 22:53:21 +0800

    feat: remove install in OneKey bat

--- OneKeyStart.bat
+++ OneKeyStart.bat
@@ -2,12 +2,10 @@
 cd /d %~dp0
 if exist runtime (
     echo Using runtime folder...
-    runtime\python.exe install.py
     runtime\python.exe -m streamlit run st.py
 ) else (
     echo Runtime folder not found. Using conda environment...
     call activate videolingo
-    python install.py
     python -m streamlit run st.py
     call deactivate
 )
--- i18n/中文/一键启动.bat
+++ i18n/中文/一键启动.bat
@@ -2,12 +2,10 @@
 cd /d %~dp0
 if exist runtime (
     echo 使用 runtime 文件夹...
-    runtime\python.exe install.py
     runtime\python.exe -m streamlit run st.py
 ) else (
     echo 未找到 runtime 文件夹，使用 conda 环境，若启动失败说明 conda 不在系统环境中...
     call activate videolingo
-    python install.py
     python -m streamlit run st.py
     call deactivate
 )
