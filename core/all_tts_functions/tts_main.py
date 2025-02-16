a22163cb6f1549f394ff67fdfda37d03aa3e5a59
Huanyu <xuhuanyu2069@gmail.com>
2025-02-16 10:34:47 +0800

    rename sf fish tts

--- core/all_tts_functions/tts_main.py
+++ core/all_tts_functions/tts_main.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from core.config_utils import load_key
 from core.all_whisper_methods.audio_preprocess import get_audio_duration
 from core.all_tts_functions.gpt_sovits_tts import gpt_sovits_tts_for_videolingo
-from core.all_tts_functions.siliconflow_fish_tts import siliconflow_fish_tts_for_videolingo
+from core.all_tts_functions.sf_fishtts import siliconflow_fish_tts_for_videolingo
 from core.all_tts_functions.openai_tts import openai_tts
 from core.all_tts_functions.fish_tts import fish_tts
 from core.all_tts_functions.azure_tts import azure_tts
