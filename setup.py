11048cc4a78e2a36e657224566305ff7bb1a1ad9
HUANYU XU <team@viora.land>
2026-09-15 06:36:53 +0000

    chore: bump version to 3.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'VideoLingo'
-VERSION = '3.0.3'
+VERSION = '3.0.4'
 
 with open('requirements.txt', encoding='utf-8') as f:
     requirements = f.read().splitlines()
